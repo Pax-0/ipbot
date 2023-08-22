4de0feb5d2b30b40535ab579b84ff5960655076d
Pax <37164780+Pax-0@users.noreply.github.com>
2023-08-23 02:06:48 +0300

    revert date format

--- src/commands/misc/ip.js
+++ src/commands/misc/ip.js
@@ -64,7 +64,6 @@ module.exports = class PingCommand extends BaseSlashCommand {
   }
   createEmbedFromIPInfo(data) {
     let embed = new EmbedBuilder();
-    let date = new Date(data.portable.timezone.current_time);
     embed.setTitle(`${data.country.name} | ${data.country.tourism_slogan}`);
     //embed.setColor('RANDOM');
     embed.setAuthor({
@@ -85,7 +84,7 @@ module.exports = class PingCommand extends BaseSlashCommand {
           : 'N/A'
       }\n**TimeZone**: ${data.portable.timezone.name} | **UTC offset** ${
         data.portable.timezone.utc_offsetStr
-      }\n**Local Time**: ${date}`
+      }\n**Local Time**: ${data.portable.timezone.current_time}`
     );
     embed.addFields([
       { name: ' ', value: ' ' },
